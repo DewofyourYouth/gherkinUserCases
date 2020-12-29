2bc415a6aeddbc7d3829e5d7d7cd18b0cb8ee47a
Jacob Shore <jshore@datalign.net>
2020-12-29 11:43:42 +0200

    backticks and comments

--- gherkinUserCases.go
+++ gherkinUserCases.go
@@ -9,7 +9,10 @@ import (
 	"strings"
 )
 
+// PermissionLevel is an int that represent the level of access to chs.care
 type PermissionLevel int
+
+// UserLevel is an type (struct) that represents a user story in gherkin
 type UserLevel struct {
 	Name     string
 	Level    PermissionLevel
@@ -18,6 +21,7 @@ type UserLevel struct {
 	AndSlice []string
 }
 
+// PermissionLevels for chs.care
 const (
 	PHYSICIAN PermissionLevel = iota
 	FACILITY
@@ -28,10 +32,10 @@ const (
 )
 
 const gherkinTemplate = ` User with {{.Name}} level access {{.Scenario}}
-  GIVEN I am a user with {{.Name}} access level
-  WHEN I {{.Scenario}}
-  THEN I {{.Result}}
-  {{if gt (len .AndSlice) 0}}{{ range  .AndSlice }}AND {{.}} 
+  ` + "`GIVEN`" + ` I am a user with {{.Name}} access level
+  ` + "`WHEN`" + ` I {{.Scenario}}
+  ` + "`THEN`" + ` I {{.Result}}
+  {{if gt (len .AndSlice) 0}}{{ range  .AndSlice }}` + "`AND`" + ` {{.}} 
   {{end}}{{end}}
 
 `
